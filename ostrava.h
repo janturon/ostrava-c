3b22c8f49103832c628304b5c82de490e557284d
Jan Turon <janturon@email.cz>
2022-06-26 16:02:46 +0200

    first commit

---
+++ ostrava.h
@@ -0,0 +1,30 @@
+#define robota int main()
+#define pyco ;
+#define gyzde ;
+#define gizde ;
+#define kurva ;
+#define je ==
+#define neni !=
+#define zgrupuj_po %
+#define nula 0
+#define do_aleluja 2000000000
+#define krat
+#define ted_tam_najebu
+
+#define dryst(name) char name[80];
+#define cyslo int
+#define chachar char
+
+#define najebej_do_konzole printf
+#define cekej getchar()
+#define odradkuj puts("")
+#define vydyndej_od_prazaka(name) scanf("%79[^\n]", name)
+
+#define rubej(N) for(int sichta=0; sichta<N; ++sichta)
+#define normalni(N) N+1
+#define jak if
+#define tak
+#define jinak else
+#define ser_na_to continue
+#define chuj_s_tym continue
+#define di_do_pici break
